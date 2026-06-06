f2d1badfa9bca828ad514369b93d3235620428cc
lochanjangid <lochanjangidcoder@gmail.com>
2026-06-07 01:38:00 +0530

    initilizing all schema into sqliter

---
+++ db/initializer.py
@@ -0,0 +1,25 @@
+import os
+from sqliter import Database, DatabaseError
+
+def init_database():
+    """Run all sql Schema Query from the CREATE TABLES files."""
+    DB_DIR = os.path.dirname(os.path.abspath(__file__))
+    SCHEMA_DIR = os.path.join(DB_DIR, "CREATE_TABLES")
+
+    DB_PATH = "school_management.db"
+    db = Database(DB_PATH)
+    
+    schema_files = ["FEES_STRUCTURE.sql", "STAFF.sql", "STUDENTS.sql", "TRANSACTIONS.sql"]
+
+    for file in schema_files:
+        file_path = os.path.join(SCHEMA_DIR, file)
+        with open(file_path, "r") as f:
+            sql_script = f.read()
+        
+            ## Execute DDL Commands
+            try:
+                db.query(sql_script)
+            except DatabaseError as e:
+                print(e)
+            else:
+                print("Done... 🐯")
